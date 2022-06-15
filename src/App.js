0cc5cd112a5ca9a8f4b0f7c4aa2d42ed8b451fd2
Ryan Robinson <ryan96663@googlemail.com>
2022-06-15 13:32:59 +0100

    refactor the main js app into components, add styles and html.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is deplyed to AWS Amplify!!
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HotelInfo from './components/Main/HotelInfo';
+import Welcome from './components/Main/Welcome';
 
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Header />    
+
+        <main id="wrapper">
+        <Welcome/>
+        <HotelInfo />
+        </main>      
+
+        <Footer />
+      </div>
+    );
+  }
+}
 export default App;
